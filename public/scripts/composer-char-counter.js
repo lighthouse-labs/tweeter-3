f27ceb2f62f117498bb8a9d10e2119fa64c7c4bc
Jaskarn <jaskarn15@gmail.com>
2022-09-29 04:30:15 +0000

    update charactoer counter code

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,21 @@
+$(document).ready(() => {
+
+  $(".tweet-text").on("input", function(e) {
+    let maxLength = 140;
+    let inputLength = $(this).val().length;
+    let charLength = maxLength - inputLength;
+
+    //let $charElement = $(this).siblings().children(".counter");
+
+    $(this).siblings().children(".counter").val(charLength);
+
+    //$charElement.text(charLength);
+
+    if(charLength < 0) {
+      $(this).siblings().children(".counter").addClass("red");
+    } else {
+      $(this).siblings().children(".counter").removeClass("red");
+    }
+  });
+}
+);
